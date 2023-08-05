126822f371e9052514969348e65f2872e6295204
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-08-05 12:14:28 +0300

    add more eslint rules and husky

--- src/logger/logger.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LogService } from './logger.service';
-
-@Module({
-  imports: [LogService],
-  exports: [LogService],
-})
-export class LoggerModule {}
